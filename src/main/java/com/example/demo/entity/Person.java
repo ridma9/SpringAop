36522006ff9287a2e04be90dcbe9a5bd05b625f6
Ridma <ridmakaveendra7@gmail.com>
2022-12-14 15:06:30 +0530

    Unit Testing Person Class

--- src/main/java/com/example/demo/entity/Person.java
+++ src/main/java/com/example/demo/entity/Person.java
@@ -18,11 +18,5 @@ public class Person {
     private String name;
     private int age;
 
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-
 
 }
---
+++ src/test/java/com/example/demo/entity/PersonTest.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+    Person person = new Person("1","Max",25);
+
+    @Test
+    void getName() {
+        assertEquals("Max",person.getName());
+    }
+
+    @Test
+    void getAge() {
+        assertEquals(25,person.getAge());
+    }
+
+    @Test
+    void setName() {
+        person.setName("Ben");
+        assertEquals("Ben",person.getName());
+    }
+
+    @Test
+    void setAge() {
+        person.setAge(20);
+        assertEquals(20,person.getAge());
+    }
+}
